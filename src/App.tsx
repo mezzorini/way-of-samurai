747fce65aaaf7aac90c512a6d11eb4f1bdcf745f
unknown <mezzorini@gmail.com>
2020-06-27 02:07:47 +0300

    start to use git

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className='app-wrapper'>
+            <header className='header'>
+                <img src="https://c7.hotpng.com/preview/845/302/525/javascript-logo-html-comment-blog-others.jpg"/>
+            </header>
+            <nav className='nav'>
+                <div>
+                    <a>Profile</a>
+                </div>
+                <div>
+                    <a>Messages</a>
+                </div>
+                <div>
+                    <a>News</a>
+                </div>
+                <div>
+                    <a>Music</a>
+                </div>
+                <div>
+                    <a>Settings</a>
+                </div>
+            </nav>
+            <div className='content'>
+                <div>
+                    <img src='https://i.pinimg.com/originals/f8/a4/c9/f8a4c90d00ce2e5684c3bf47c4a6ce17.jpg'/>
+                </div>
+                <div>
+                    <img src='https://klike.net/uploads/posts/2019-03/1551511784_4.jpg'/>
+                </div>
+                <div>
+                    my posts
+                    <div>
+                        new posts
+                    </div>
+                    <div>
+                        post 1
+                    </div>
+                    <div>
+                        post 2
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
